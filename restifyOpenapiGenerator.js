6cf42bd3875d0686d2257c36bb69832972b626a6
Nikolai Ovtsinnikov <nikolai@zone.ee>
2024-11-28 10:24:58 +0200

    add path module!

--- restifyOpenapiGenerator.js
+++ restifyOpenapiGenerator.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const structuredCloneWrapper = typeof structuredClone === 'function' ? structuredClone : obj => JSON.parse(JSON.stringify(obj));
 
